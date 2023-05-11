b9f32513f04189a2e37a84cbfb27fd2bb9b86a5d
Krazy885 <125690939+Krazy885@users.noreply.github.com>
2023-05-11 14:02:14 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+settings = {
+    'token': 'MTEwMjY3OTYwNDg0NzcyMjcyNg.GeoAV_.KpNDZc12bDe7Lr9RMLHgy8aICTo79-e_69j8J4',
+    'bot': 'Боба.py',
+    'prefix': "!",
+    'welcome_role_id': '1079792436940918853',
+    'welcome_channel_id': '1079792437339357217',
+    'Voice_channel': '1100765996085678120',
+    'guild_id': '1079792436727009280',
+    'default_color': '0x4e77eb'
+
+
+
+
+
+
+}
