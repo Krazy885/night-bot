8bbb95827bd400bc9870a3c801f12e60d7502d85
Krazy885 <125690939+Krazy885@users.noreply.github.com>
2023-05-11 14:03:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'MTEwMjY3OTYwNDg0NzcyMjcyNg.GeoAV_.KpNDZc12bDe7Lr9RMLHgy8aICTo79-e_69j8J4',
+    'token': 'token',
     'bot': 'Боба.py',
     'prefix': "!",
     'welcome_role_id': '1079792436940918853',
